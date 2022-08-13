e6508296a8c1e235e7695a9a5519a14706ae4d75
O-Schell <59923444+O-Schell@users.noreply.github.com>
2022-08-14 00:09:44 +0200

    implemented: Kill script by discord message; Last Message; Removed annoying updating modules and script messages

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import { env } from 'node:process';
-import { readdirSync } from 'node:fs';
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { readdirSync, existsSync, writeFile, mkdir, writeFileSync, readFileSync } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { Client, Collection, GatewayIntentBits, IntentsBitField } from 'discord.js';
 import dotenv from 'dotenv';
 import Members from './module/flux/include/Members/index.js';
 import update from './utils/update.js';
 import { searchJSCommand } from './utils/utils.js';
 import { art, info, activity } from './module/functions/Presentation.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 dotenv.config();
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const Intents = new IntentsBitField(3276799);
+//3276799
+//[GatewayIntentBits.Guilds]
+const client = new Client({ intents: Intents });
 
 client.commands = new Collection();
 
 art();
 
-update();
-
+update()
 
 try {
   await searchJSCommand('module/commands/include', !1, (module) => {
@@ -45,6 +52,51 @@ try {
       });
     }
   });
+
+  client.on('messageCreate', async (message) => {
+    if (message.content == "stop" && message.author.id == "145525624939741184" || message.content == "stop" && message.author.id == "331778741917319168") {
+      await message.reply(":robot: :zzz:");
+      console.log("🤖 💤");
+      process.exit(0);
+    }
+
+    
+
+    var GUILD = message.guildId;
+    var AUTHOR = message.author.id; 
+    var TIMESTAMP = message.createdTimestamp;
+    
+    var dirPath = join(__dirname, `/data/lm`);
+    var filePath = join(dirPath, `/${GUILD}.json`);
+
+    if (existsSync(dirPath)) {
+      //console.log("Directory exists !");
+    } else {
+      //console.log("Directory not found.");
+      mkdir(dirPath, function (err) {
+        if (err) throw err;
+        //console.log("Directory is created.");
+      });
+    }
+
+    if (existsSync(filePath)) {
+      //console.log(`${GUILD}.json exist`);
+    } else {
+      //console.log(`${GUILD}.json don't exist`);
+      writeFile(filePath, "{}", function (err) {
+        if (err) throw err;
+        //console.log("File is created successfully.");
+      });
+    }
+
+    const data = JSON.parse(readFileSync(filePath));
+    data[AUTHOR] = `${Number(TIMESTAMP)}`;
+    writeFileSync(
+      filePath,
+      JSON.stringify(data, null, 4)
+    );
+
+  })
 
   Members.join(client);
   Members.exit(client);
--- src/utils/update.js
+++ src/utils/update.js
@@ -1,51 +1,53 @@
-import { get } from 'node:https';
-import { env } from 'node:process';
-import { exec } from 'node:child_process';
+import { get } from "node:https";
+import { env } from "node:process";
+import { exec } from "node:child_process";
 
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
-const update = () => {
+const update = async () => {
   var data;
 
   get(
-    'https://raw.githubusercontent.com/France-Alliance/Bot-Discord-V2/master/package.json',
+    "https://raw.githubusercontent.com/France-Alliance/Bot-Discord-V2/master/package.json",
     (res) => {
-      res.on('data', async (chunk) => {
+      res.on("data", async (chunk) => {
         data = JSON.parse(chunk);
 
         if (
           (data.version[0] !== env.VERSION[0] ||
             data.version[2] !== env.VERSION[2] ||
             data.version[4] !== env.VERSION[4]) &&
-          env.DEV !== '1'
+          env.DEV !== "1"
         ) {
-          console.log(
+          /* console.log(
             `[Update] - Version ${data.version} of the bot is available !`
-          );
-          console.log(`[Update] - Updateing...`);
-          execFile('git fetch --all -P');
-          console.log(`[Update] - Update done !`);
+          ); */
+
+          /*console.log(`[Update] - Updating...`);*/
+          execFile("git fetch --all -P");
+          /*console.log(`[Update] - Update done !`);*/
         } else {
-          console.log(`[Update] - You used the latest version of the bot !`);
+          /*console.log(`[Update] - You used the latest version of the bot !`);*/
+
         }
 
-        const packageUpdate = exec('npm outdated --json', {
-          encoding: 'utf8',
+        const packageUpdate = exec("npm outdated --json", {
+          encoding: "utf8",
         });
-        packageUpdate.stdout.on('data', async (data) => {
+        packageUpdate.stdout.on("data", async (data) => {
           data = JSON.parse(data);
 
           if (data !== {}) {
-            console.log(`[Update] - Some dependencies are not up to date !`);
-            console.log(`[Update] - Updateing...`);
-            exec('pnpm update');
-            packageUpdate.on('exit', () => {
-              console.log(`[Update] - Update done !`);
+            /*console.log(`[Update] - Some dependencies are not up to date !`);
+            console.log(`[Update] - Updateing...`);*/
+            exec("pnpm update");
+            packageUpdate.on("exit", () => {
+              /*console.log(`[Update] - Update done !`);*/
             });
           } else {
-            console.log(`[Update] - All dependencies are up to date !`);
+            /*console.log(`[Update] - All dependencies are up to date !`);*/
           }
         });
       });
